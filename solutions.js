b7d558ca1a0bf869265600fbf78bd6f1e11c608f
alinelmuller <alinelmuller@gmail.com>
2024-10-28 14:46:19 -0600

    solve fourth  problem

--- solutions.js
+++ solutions.js
@@ -1,6 +1,6 @@
 /*************************************************
- * Exercise 1: Applying Array.prototype.map()
- ************************************************/
+* Exercise 1: Applying Array.prototype.map()
+************************************************/
 
 // Use `.map()` to iterate over the following array:
 const nums = [13, 87, 2, 89, 12, 4, 90, 63];
@@ -15,7 +15,7 @@ console.log(multipliedNums);
 
 
 /*************************************************
- * Exercise 2: Array destructuring
+* Exercise 2: Array destructuring
 ************************************************/
 
 // Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
@@ -29,7 +29,7 @@ console.log(secondTopping); // 'Snuffles'
 
 
 /*************************************************
- * Exercise 3: Destructuring objects
+* Exercise 3: Destructuring objects
 ************************************************/
 
 // Given the following object, use destructuring to create variables `make` and `model` that will hold 
@@ -46,4 +46,16 @@ const { make, model } = car;
 console.log(make);
 console.log(model);
 
+/******************************************************
+* Exercise 4: Applying the spread operator on arrays
+******************************************************/
   
+// Duplicate the following array using the spread operator and assign it to `controversialPizzaToppings`. 
+//Then, log the variable.
+const pizzaToppings2 = ['Pineapple', 'Olives', 'Anchovies'];
+
+// Your code here
+const controversialPizzaToppings = [...pizzaToppings2];
+
+console.log(controversialPizzaToppings);
+
